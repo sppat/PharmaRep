3f88044b6a35a19c96cd43a750e6d3e4b41a79e4
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-03-23 11:57:44 +0200

    minor refactor in UserArgumentException

--- PharmaRep.BE/src/Modules/Identity/Identity.Domain/Exceptions/UserExceptions/UserArgumentException.cs
+++ PharmaRep.BE/src/Modules/Identity/Identity.Domain/Exceptions/UserExceptions/UserArgumentException.cs
@@ -1,6 +1,3 @@
 namespace Identity.Domain.Exceptions.UserExceptions;
 
-public class UserArgumentException(string message, string paramName) : ArgumentException(message, paramName)
-{
-    
-}
+public class UserArgumentException(string message, string paramName) : ArgumentException(message, paramName);
