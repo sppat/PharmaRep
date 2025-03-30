34f2cafba80b6b9c5045c96bc4670f3c2496d983
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-03-30 09:33:36 +0300

    finalize register endpoint integration tests

--- PharmaRep.BE/src/Modules/Identity/Identity.Domain/DomainErrors/IdentityModuleDomainErrors.cs
+++ PharmaRep.BE/src/Modules/Identity/Identity.Domain/DomainErrors/IdentityModuleDomainErrors.cs
@@ -7,8 +7,8 @@ public static class IdentityModuleDomainErrors
         public const string InvalidEmail = "Invalid user email";
         public const string InvalidFirstName = "Invalid user first name";
         public const string InvalidLastName = "Invalid user last name";
-        public const string NameOutOfRange = "Name is out of range";
-        public const string EmailOutOfRange = "Email is out of range";
+        public const string NameOutOfRange = "Name length is out of range";
+        public const string EmailOutOfRange = "Email length is out of range";
         public const string InvalidPassword = "Invalid password";
         public const string EmptyRoles = "Empty roles";
         public const string InvalidRole = "Invalid role";
--- PharmaRep.BE/tests/ModuleTests/IdentityTests/Identity.WebApi.Tests/Endpoints/UserEndpointsTests.cs
+++ PharmaRep.BE/tests/ModuleTests/IdentityTests/Identity.WebApi.Tests/Endpoints/UserEndpointsTests.cs
@@ -30,7 +30,9 @@ public class UserEndpointsTests(WebApplicationFixture fixture) : IClassFixture<W
         Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         Assert.NotEqual(Guid.Empty, responseContent.UserId);
     }
-
+    
+    #region FirstName
+    
     [Theory]
     [InlineData("")]
     [InlineData("  ")]
@@ -51,6 +53,178 @@ public class UserEndpointsTests(WebApplicationFixture fixture) : IClassFixture<W
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         ProblemDetailsAssert.HasErrors(expectedErrors, errors);
     }
+    
+    [Fact]
+    public async Task RegisterUser_FirstNameExceedsMaxLength_ReturnBadRequest()
+    {
+        // Arrange
+        var firstNameAsEnumerable = Enumerable.Repeat("a", 101);
+        var firstName = string.Join(string.Empty, firstNameAsEnumerable);
+        var request = TestEnvironment.ValidRegisterUserRequest with { FirstName = firstName };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.NameOutOfRange };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+    
+    #endregion
+
+    #region LastName
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    [InlineData("12a")]
+    public async Task RegisterUser_InvalidLastName_ReturnBadRequest(string lastName)
+    {
+        // Arrange
+        var request = TestEnvironment.ValidRegisterUserRequest with { LastName = lastName };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.InvalidLastName };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+    
+    [Fact]
+    public async Task RegisterUser_LastNameExceedsMaxLength_ReturnBadRequest()
+    {
+        // Arrange
+        var lastNameAsEnumerable = Enumerable.Repeat("a", 101);
+        var lastName = string.Join(string.Empty, lastNameAsEnumerable);
+        var request = TestEnvironment.ValidRegisterUserRequest with { LastName = lastName };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.NameOutOfRange };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+
+    #endregion
+
+    #region Email
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    [InlineData("12a")]
+    [InlineData("a@b .c")]
+    public async Task RegisterUser_InvalidEmail_ReturnBadRequest(string email)
+    {
+        // Arrange
+        var request = TestEnvironment.ValidRegisterUserRequest with { Email = email };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.InvalidEmail };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+    
+    [Fact]
+    public async Task RegisterUser_EmailExceedsMaxLength_ReturnBadRequest()
+    {
+        // Arrange
+        var emailAsEnumerable = Enumerable.Repeat("a", 101);
+        var email = string.Join(string.Empty, emailAsEnumerable);
+        var request = TestEnvironment.ValidRegisterUserRequest with { Email = email };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.EmailOutOfRange };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+
+    #endregion
+
+    #region Password
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public async Task RegisterUser_InvalidPassword_ReturnBadRequest(string password)
+    {
+        // Arrange
+        var request = TestEnvironment.ValidRegisterUserRequest with { Password = password};
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.InvalidPassword };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+
+    #endregion
+
+    #region Roles
+
+    [Fact]
+    public async Task RegisterUser_EmptyRoles_ReturnBadRequest()
+    {
+        // Arrange
+        var request = TestEnvironment.ValidRegisterUserRequest with { Roles = [] };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.EmptyRoles };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+    
+    [Fact]
+    public async Task RegisterUser_InvalidRole_ReturnBadRequest()
+    {
+        // Arrange
+        var request = TestEnvironment.ValidRegisterUserRequest with { Roles = [ Role.Doctor.Name, "TestRole" ] };
+        var expectedErrors = new[] { IdentityModuleDomainErrors.UserErrors.InvalidRole };
+
+        // Act
+        var response = await _httpClient.PostAsJsonAsync(IdentityModuleUrls.User.Register, request);
+        var responseContent = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+        var errors = responseContent.GetErrors();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        ProblemDetailsAssert.HasErrors(expectedErrors, errors);
+    }
+
+    #endregion
 
     #endregion
 
