a64fbe50068cbe05a0e413f180be75e0901925f3
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-04-13 00:01:44 +0300

    add user repository

---
+++ PharmaRep.BE/src/Modules/Identity/Identity.Infrastructure/Mappings/UserMappings.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Application.Features.User.Dtos;
+using Identity.Domain.Entities;
+
+namespace Identity.Infrastructure.Mappings;
+
+public static class UserMappings
+{
+    public static UserDto ToUserDto(this User user) => new(user.Id,
+        user.FirstName,
+        user.LastName,
+        user.Email,
+        user.UserRoles.Select(r => r.Role.Name));
+}
---
+++ PharmaRep.BE/src/Modules/Identity/Identity.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Identity.Application.Features.User.Dtos;
+using Identity.Application.Features.User.Interfaces;
+using Identity.Infrastructure.Database;
+using Identity.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Repositories;
+
+public class UserRepository(PharmaRepIdentityDbContext dbContext) : IUserRepository
+{
+    public async Task<UserDto> GetUserAsync(Guid userId, CancellationToken cancellationToken)
+        => await dbContext.Users.Include(u => u.UserRoles)
+            .ThenInclude(ur => ur.Role)
+            .Where(u => u.Id == userId)
+            .Select(u => u.ToUserDto())
+            .SingleOrDefaultAsync(cancellationToken);
+}
