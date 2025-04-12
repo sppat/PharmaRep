15f434d15f5485ada7bb869584a04daf6ee837f5
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-04-13 00:56:41 +0300

    add swagger

--- PharmaRep.BE/src/Bootstrapper/Bootstrapper/Program.cs
+++ PharmaRep.BE/src/Bootstrapper/Bootstrapper/Program.cs
@@ -4,12 +4,20 @@ using Shared.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
 builder.Services.AddDispatcher();
 builder.Services.AddIdentityWebApi(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
