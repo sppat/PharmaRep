d6db60ff35fddd06bb7fd94be2ccac5fda9f42a2
Spyros Paterakis <s.paterakis@outlook.com.gr>
2025-04-21 17:26:20 +0300

    fix paginated response tests

--- PharmaRep.BE/tests/ModuleTests/IdentityTests/Identity.WebApi.Tests/PaginatedResponseTests.cs
+++ PharmaRep.BE/tests/ModuleTests/IdentityTests/Identity.WebApi.Tests/PaginatedResponseTests.cs
@@ -6,34 +6,46 @@ namespace Identity.WebApi.Tests;
 public class PaginatedResponseTests
 {
     [Theory]
-    [InlineData(0, 5, new[] {1, 2, 3, 4, 5})]
-    [InlineData(-1, 5, new[] {1, 2, 3, 4, 5})]
-    public void Create_PageNumberNonPositive_ThrowsException(int pageNumber, int pageSize, int[] items)
+    [InlineData(0, 5, 5, new[] {1, 2, 3, 4, 5})]
+    [InlineData(-1, 5, 5, new[] {1, 2, 3, 4, 5})]
+    public void Create_PageNumberNonPositive_ThrowsException(int pageNumber, int pageSize, int total, int[] items)
     {
         // Arrange
-        PaginatedResponse<int> Create() => new(pageNumber, pageSize, items);
+        PaginatedResponse<int> Create() => new(pageNumber, pageSize, total, items);
         
         // Assert
         Assert.Throws<ArgumentOutOfRangeException>(nameof(pageNumber), Create);
     }
     
     [Theory]
-    [InlineData(1, 0, new[] {1, 2, 3, 4, 5})]
-    [InlineData(1, -1, new[] {1, 2, 3, 4, 5})]
-    public void Create_PageSizeNonPositive_ThrowsException(int pageNumber, int pageSize, int[] items)
+    [InlineData(1, 0, 5, new[] {1, 2, 3, 4, 5})]
+    [InlineData(1, -1, 5, new[] {1, 2, 3, 4, 5})]
+    public void Create_PageSizeNonPositive_ThrowsException(int pageNumber, int pageSize, int total, int[] items)
     {
         // Arrange
-        PaginatedResponse<int> Create() => PaginatedResponse<int>.Create(pageNumber, pageSize, items);
+        PaginatedResponse<int> Create() => new(pageNumber, pageSize, total, items);
         
         // Assert
         Assert.Throws<ArgumentOutOfRangeException>(nameof(pageSize), Create);
+    }
+
+    [Theory] 
+    [InlineData(1, 5, 0, new[] { 1, 2, 3, 4, 5 })] 
+    [InlineData(1, 5, -1, new[] { 1, 2, 3, 4, 5 })]
+    public void Create_TotalNonPositive_ThrowsException(int pageNumber, int pageSize, int total, int[] items)
+    {
+        // Arrange
+        PaginatedResponse<int> Create() => new(pageNumber, pageSize, total, items);
+        
+        // Assert
+        Assert.Throws<ArgumentOutOfRangeException>(nameof(total), Create);
     }
     
     [Fact]
     public void Create_NullItems_ThrowsException()
     {
         // Arrange
-        PaginatedResponse<int> Create() => PaginatedResponse<int>.Create<int>(pageNumber: 1, pageSize: 5, items: null);
+        PaginatedResponse<int> Create() => new(pageNumber: 1, pageSize: 5, total: 5, items: null);
         
         // Assert
         Assert.Throws<ArgumentNullException>(paramName: "items", Create);
@@ -46,9 +58,10 @@ public class PaginatedResponseTests
         const int pageNumber = 1;
         const int pageSize = 3;
         var items = new[] {1, 2, 3, 4, 5};
+        var total = items.Length;
         
         // Act
-        var response = PaginatedResponse<int>.Create(pageNumber, pageSize, items);
+        var response = new PaginatedResponse<int>(pageNumber, pageSize, total, items);
         
         // Assert
         Assert.False(response.HasPrevious);
@@ -59,11 +72,12 @@ public class PaginatedResponseTests
     {
         // Arrange
         var items = new[] {1, 2, 3, 4, 5};
+        var total = items.Length;
         const int pageSize = 3;
         var pageNumber = (int)Math.Ceiling((double)items.Length / pageSize);
         
         // Act
-        var response = PaginatedResponse<int>.Create(pageNumber, pageSize, items);
+        var response = new PaginatedResponse<int>(pageNumber, pageSize, total, items);
         
         // Assert
         Assert.False(response.HasNext);
@@ -74,11 +88,12 @@ public class PaginatedResponseTests
     {
         // Arrange
         var items = new[] {1, 2, 3, 4, 5};
+        var total = items.Length;
         const int pageSize = 2;
         var pageNumber = (int)Math.Floor((double)items.Length / pageSize);
         
         // Act
-        var response = PaginatedResponse<int>.Create(pageNumber, pageSize, items);
+        var response = new PaginatedResponse<int>(pageNumber, pageSize, total, items);
         
         // Assert
         Assert.True(response.HasPrevious);
