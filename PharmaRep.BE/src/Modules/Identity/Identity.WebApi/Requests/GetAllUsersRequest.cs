741f15aacb58bd06f0679c0f3e109296696ee4f9
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-04-22 19:14:52 +0300

    add default values for GetAllUsersRequest

--- PharmaRep.BE/src/Modules/Identity/Identity.WebApi/Requests/GetAllUsersRequest.cs
+++ PharmaRep.BE/src/Modules/Identity/Identity.WebApi/Requests/GetAllUsersRequest.cs
@@ -2,4 +2,4 @@ using Shared.WebApi.Requests;
 
 namespace Identity.WebApi.Requests;
 
-public record GetAllUsersRequest(int PageNumber, int PageSize) : PaginationRequest(PageNumber, PageSize);
+public record GetAllUsersRequest(int PageNumber = 1, int PageSize = 10) : PaginationRequest(PageNumber, PageSize);
