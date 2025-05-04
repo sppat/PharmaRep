91e6b00f523d3b51788b4ca872286c7672213533
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-05-04 17:37:30 +0300

    render a dummy list of users

---
+++ PharmaRep.Admin/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        { id: 1, name: 'John Doe', age: 30 },
+        { id: 2, name: 'Jane Smith', age: 25 },
+        { id: 3, name: 'Alice Johnson', age: 28 },
+      ],
+    }
+  },
+}
+</script>
+
+<template>
+  <section>
+    <h2>Users</h2>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <p>Name: {{ user.name }}</p>
+        <p>Age: {{ user.age }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
