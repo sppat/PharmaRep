188ccefef59f0606b13d60e7a2512ca82da235d1
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-05-05 08:03:05 +0300

    add rule in delete user command validator

--- PharmaRep.BE/src/Modules/Identity/Identity.Application/Features/User/Delete/DeleteUserCommandValidator.cs
+++ PharmaRep.BE/src/Modules/Identity/Identity.Application/Features/User/Delete/DeleteUserCommandValidator.cs
@@ -4,5 +4,8 @@ namespace Identity.Application.Features.User.Delete;
 
 public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
 {
-    
+    public DeleteUserCommandValidator()
+    {
+        RuleFor(cmd => cmd.UserId).NotEmpty();
+    }
 }
