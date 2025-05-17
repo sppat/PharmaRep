47310b22ae44eb229856f7217e63bfe384fcc8f7
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-05-17 20:28:14 +0300

    add links to users and events pages

--- PharmaRep.Admin/src/app/sidebar/sidebar.component.ts
+++ PharmaRep.Admin/src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
