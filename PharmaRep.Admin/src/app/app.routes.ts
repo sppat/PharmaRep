1a88df682cb5d4e60dbfead4d0d7cc59a0daffa2
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-05-17 20:23:56 +0300

    add routes and wrap router-outlet

--- PharmaRep.Admin/src/app/app.routes.ts
+++ PharmaRep.Admin/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+import { EventListComponent } from './event-list/event-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'events', component: EventListComponent },
+];
