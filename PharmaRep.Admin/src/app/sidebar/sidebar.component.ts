bd439e19a00ee1732c0b3073fecfddac4f8d5610
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-05-17 20:10:41 +0300

    add layout

---
+++ PharmaRep.Admin/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [MatIconModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
