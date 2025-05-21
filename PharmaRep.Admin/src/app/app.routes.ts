d30247008e1d787b1de9cb1dfea97d228a153032
Spyros Paterakis <s.paterakis@kaizengaming.com>
2025-05-21 19:13:42 +0300

    add users list

--- PharmaRep.Admin/src/app/app.routes.ts
+++ PharmaRep.Admin/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { EventListComponent } from './event-list/event-list.component';
+import { UserComponent } from './user-list/user/user.component';
 
 export const routes: Routes = [
   { path: 'users', component: UserListComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'events', component: EventListComponent },
 ];
--- PharmaRep.Admin/src/app/user-list/user-list.component.ts
+++ PharmaRep.Admin/src/app/user-list/user-list.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { User } from '../models/user';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
 export class UserListComponent {
-
+  users: User[] = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@doe.com',
+      roles: [{ id: 2, name: 'Doctor' }],
+    },
+    {
+      id: 2,
+      firstName: 'John',
+      lastName: 'Foo',
+      email: 'john@foo.com',
+      roles: [{ id: 2, name: 'Medical Representative' }],
+    },
+  ];
 }
