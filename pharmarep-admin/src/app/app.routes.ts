1b0b2eba887575d5471d23d67136229e9722f6da
Spyros Paterakis <s.paterakis@outlook.com.gr>
2025-11-01 20:39:46 +0200

    add router outlet

--- pharmarep-admin/src/app/app.routes.ts
+++ pharmarep-admin/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {UsersList} from './users-list/users-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersList
+  }
+];
--- pharmarep-admin/src/app/layout/layout.ts
+++ pharmarep-admin/src/app/layout/layout.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NavBar} from '../nav-bar/nav-bar';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    NavBar
+    NavBar,
+    RouterOutlet,
+    RouterOutlet
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
