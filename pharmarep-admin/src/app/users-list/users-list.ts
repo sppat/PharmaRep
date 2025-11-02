e876cf2eb723de344fc52382762298e49da33fbc
Spyros Paterakis <s.paterakis@outlook.com.gr>
2025-11-02 11:51:16 +0200

    create html and css for users list

--- pharmarep-admin/src/app/users-list/users-list.ts
+++ pharmarep-admin/src/app/users-list/users-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-users-list',
@@ -7,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrl: './users-list.css',
 })
 export class UsersList {
-
+  users = [
+    {
+      "firstName": "Alice",
+      "lastName": "Johnson",
+      "email": "alice@johnson.com"
+    },
+    {
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "bob@smith.com"
+    },
+    {
+      "firstName": "Charlie",
+      "lastName": "Brown",
+      "email": "charlie@brown.com"
+    },
+    {
+      "firstName": "Diana",
+      "lastName": "Miller",
+      "email": "diana@miller.com"
+    },
+    {
+      "firstName": "Ethan",
+      "lastName": "Davis",
+      "email": "ethan@davis.com"
+    },
+    {
+      "firstName": "Fiona",
+      "lastName": "Wilson",
+      "email": "fiona@wilson.com"
+    },
+    {
+      "firstName": "George",
+      "lastName": "Taylor",
+      "email": "george@taylor.com"
+    },
+    {
+      "firstName": "Hannah",
+      "lastName": "Moore",
+      "email": "hannah@moore.com"
+    },
+    {
+      "firstName": "Ian",
+      "lastName": "Anderson",
+      "email": "ian@anderson.com"
+    },
+    {
+      "firstName": "Julia",
+      "lastName": "Thomas",
+      "email": "julia@thomas.com"
+    }
+  ];
 }
