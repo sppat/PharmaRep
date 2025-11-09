f4977e97d8a866c65230bd000fc4f0a8798ae206
Spyros Paterakis <s.paterakis@outlook.com.gr>
2025-11-09 17:07:54 +0200

    use routerLink for proper routing

--- pharmarep-admin/src/app/nav-bar/nav-bar-item/nav-bar-item.ts
+++ pharmarep-admin/src/app/nav-bar/nav-bar-item/nav-bar-item.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar-item',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './nav-bar-item.html',
   styleUrl: './nav-bar-item.css',
 })
 export class NavBarItem {
-  @Input({ required: true }) image!: string;
-  @Input({ required: true }) alt!: string;
+  @Input({required: true}) image!: string;
+  @Input({required: true}) alt!: string;
 
   get imagePath() {
     return "/menuIcons/" + this.image;
