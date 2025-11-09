a6a71108add850badb6e873bb3a312c8207eb828
Spyros Paterakis <s.paterakis@outlook.com.gr>
2025-11-09 17:00:14 +0200

    redirect to users when accessing home path

--- pharmarep-admin/src/app/app.routes.ts
+++ pharmarep-admin/src/app/app.routes.ts
@@ -1,7 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {UsersList} from './users-list/users-list';
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/users',
+  },
   {
     path: 'users',
     component: UsersList
