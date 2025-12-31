80f0d8ed24ff7323f3299c63bbc337184234a4f4
Spyros Paterakis <spaterakis@novibet.com>
2025-12-31 11:11:03 +0200

    minor poc for AuthenticationStateProvider

---
+++ PharmaRep.Admin/src/PharmaRep.Admin/Utils/AuthStateProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace PharmaRep.Admin.Utils;
+
+public class AuthStateProvider : AuthenticationStateProvider
+{
+	public override Task<AuthenticationState> GetAuthenticationStateAsync()
+	{
+		var identity = new ClaimsIdentity();
+		var user = new ClaimsPrincipal(identity);
+		var authenticationState = new AuthenticationState(user);
+
+		return Task.FromResult(authenticationState);
+	}
+
+	public void AuthenticateUser(string email)
+	{
+		var identity = new ClaimsIdentity(
+		[
+			new(ClaimTypes.Email, email)
+		], "Bearer");
+
+		var user = new ClaimsPrincipal(identity);
+		var authenticationState = new AuthenticationState(user);
+
+		NotifyAuthenticationStateChanged(Task.FromResult(authenticationState));
+	}
+}
