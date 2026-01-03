7f3c5c758917fe481f65d0275139e10733a749c5
Spyros Paterakis <s.paterakis@outlook.com.gr>
2026-01-03 10:50:45 +0200

    add appsettings

---
+++ PharmaRep.Admin/src/PharmaRep.Admin/Configurations/ApiClientConfiguration.cs
@@ -0,0 +1,8 @@
+namespace PharmaRep.Admin.Configurations;
+
+public sealed class ApiClientConfiguration
+{
+    public const string Section = "ApiClient";
+
+    public string BaseAddress { get; set; } = string.Empty;
+}
--- PharmaRep.Admin/src/PharmaRep.Admin/Program.cs
+++ PharmaRep.Admin/src/PharmaRep.Admin/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 using PharmaRep.Admin;
+using PharmaRep.Admin.Configurations;
 using PharmaRep.Admin.Services;
 using PharmaRep.Admin.Utils;
 
@@ -9,7 +11,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/api/") });
+builder.Services.Configure<ApiClientConfiguration>(builder.Configuration.GetSection(ApiClientConfiguration.Section));
+builder.Services.AddScoped(serviceProvider =>
+{
+    var apiClientConfiguration = serviceProvider.GetRequiredService<IOptions<ApiClientConfiguration>>()?.Value;
+    if (string.IsNullOrWhiteSpace(apiClientConfiguration?.BaseAddress))
+    {
+        throw new ArgumentNullException(nameof(ApiClientConfiguration.BaseAddress));
+    }
+
+    return new HttpClient() { BaseAddress = new Uri(apiClientConfiguration.BaseAddress) };
+});
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
